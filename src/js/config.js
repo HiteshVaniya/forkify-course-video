ec705945de330c760628d26574ccb14ac10ed5ea
HiteshVaniya <vaniyah11@gmail.com>
2023-08-14 12:27:35 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eb22fdba-5741-4fd9-999a-d7a4cd9c48ed';
+export const MODAL_CLOSE_SEC = 2.5;
